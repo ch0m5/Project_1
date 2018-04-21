3a07140e8e5152a9375143d8c29e8e36263f5400
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2018-04-21 18:09:58 +0200

    enemy greyball s movement

---
+++ Andro_Dunos/Enemy_greyball_s.h
@@ -0,0 +1,28 @@
+//#ifndef __ENEMY_13_GREYBALL_H__
+//#define __ENEMY_13_GREYBALL_H__
+//
+//#include "Enemy.h"
+//
+//class Enemy_13_Greyball : public Enemy
+//{
+//private:
+//	float wave = -1.0f;
+//	bool going_up = false;
+//	int original_y = 0;
+//	Animation fly;
+//
+//public:
+//	Enemy_13_Greyball(int x, int y);
+//	virtual void OnCollision(Collider* collider) override;
+//	// Enemy Hitbox
+//	int hitboxWidth;
+//	int hitboxHeight;
+//
+//	void Move();
+//	void Fire();
+//
+//	bool dead = false;
+//	uint score = 100;
+//};
+//
+//#endif // __ENEMY_EXAMPLE_H__
