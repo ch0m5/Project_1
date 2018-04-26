c26389768bf585922361982ff66064b6455426cc
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2018-04-26 13:35:14 +0200

    Implemented Full Screen READ DESCRIPTION
    MUST fix speed issues (HardCoded values).
    MUST fix all other Harcoded values.

--- Andro_Dunos/Globals.h
+++ Andro_Dunos/Globals.h
@@ -48,11 +48,11 @@ enum weapon_levels	// To mark the different player weapon levels
 typedef unsigned int uint;
 
 // Configuration -----------
-#define SCREEN_SIZE 3		// Multiplies the screen size and resolution
+#define SCREEN_SIZE 1		// Multiplies the screen size and resolution
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 224
 #define WIN_FULLSCREEN 0
-#define WIN_FULLSCREEN_DESKTOP 0
+#define WIN_FULLSCREEN_DESKTOP 1
 #define WIN_BORDERLESS 0
 #define WIN_RESIZABLE 0
 #define REN_VSYNC 1
